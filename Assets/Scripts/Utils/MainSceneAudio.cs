623067e9b91abf3ec14aecb137dde41be869cddf
Robert Kleszczynski <robert.ks.email@gmail.com>
2016-04-12 02:22:55 -0400

    Added Selection Scripts. Oculus Scripts toss up Compiler Errors though.

---
+++ Assets/Scripts/Utils/MainSceneAudio.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainSceneAudio : MonoBehaviour 
+{
+    public AudioSource bgm;
+    private float tparam;
+    public float fadeOffset = 3; //smooth the fade out
+    private int played = 0;
+    public static MainSceneAudio S;
+	// Use this for initialization
+	void Awake () 
+    {
+        this.GetComponent<Fading>().BeginFade(-1);
+        S = this;
+	}
+
+    void Start()
+    {
+        GetComponent<FadeMusic>().AssignTunes();
+        if (PlayerPrefs.GetInt("isPlayed") != 0)
+        {
+            played = PlayerPrefs.GetInt("isPlayed");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<FadeMusic>().FadeInTunes();
+        GetComponent<FadeMusic>().FadeOutTunes(); 
+	}
+
+    public void GameOver()
+    {
+        StartCoroutine(LoadCutScene("GameOver"));
+    }
+  public  IEnumerator LoadCutScene(string lvlIndex)
+    {
+        //fade out game and load a new level
+        GetComponent<FadeMusic>().fadeInTunes = false;
+        GetComponent<FadeMusic>().fadeOutTunes = true;
+        float fadeTime = this.GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime * fadeOffset);
+        SceneManager.LoadScene(lvlIndex);
+           
+    }
+}
