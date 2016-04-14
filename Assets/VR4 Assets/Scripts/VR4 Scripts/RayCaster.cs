3464b32967bcd7d6e6840a817e5d57d296601531
Robert Kleszczynski <robert.ks.email@gmail.com>
2016-04-14 00:13:55 -0400

    Slection Demo Scene added

---
+++ Assets/VR4 Assets/Scripts/VR4 Scripts/RayCaster.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 
+/// </summary>
+
+public class RayCaster : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject lastObjectHit;
+    bool isOff = false;
+    public static GameObject currentPlanet;                              //initialized in Gravity2 at startup
+    public LayerMask objectLayer;                                       //set in inspector
+    public float lookDist = 10;
+   
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("RayCasting", .1f, 1f / 60);
+    }
+
+    // Update is called once per frame
+    void RayCasting()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        if ( Physics.Raycast(ray, out hit, lookDist, objectLayer))                                //use SphereCast instead
+        {
+            //print("hit" + hit.transform.gameObject.name);                                                       //prints what the raycaster is hitting
+            GameObject objectHit = hit.transform.gameObject;
+
+            ObjectIDAndSelect(objectHit);
+
+
+        }
+        else
+        {
+
+            ObjectIDAndSelect(null);
+        }
+    }
+
+    void ObjectIDAndSelect(GameObject go)
+    {
+
+        if (lastObjectHit == null)                  //when we first see an object, set it up for selection
+        {
+            // print("space");
+            lastObjectHit = go;
+            isOff = false;
+        }
+        else if (go == lastObjectHit)
+        {
+            if (go.tag == "Phone" || go.tag == "Beer" || go.tag == "Water" || go.tag == "Keys")                                                                  //CHANEG THIS TO WHATEVER TAGS WE ARE USING FOR THE OBJECTS
+            {
+                // print("Charging");
+                go.GetComponentInChildren<ItemSelection>().isOver = true;
+            }
+
+
+            isOff = false;
+
+        }
+        else if (lastObjectHit != go)
+        {
+            //print("missed");
+            if (lastObjectHit != null && (lastObjectHit.tag == "Phone" || lastObjectHit.tag == "Beer" || lastObjectHit.tag == "Water" || lastObjectHit.tag == "Keys"))          //CHANGE THIS TO WHATEVER TAGS WE ARE USING FOR THE OBJECTS
+                lastObjectHit.GetComponentInChildren<ItemSelection>().isOver = false;
+
+            isOff = true;
+            ObjectCheck(go);
+        }
+
+
+        lastObjectHit = go;
+
+
+
+
+    }
+
+    void ObjectCheck(GameObject go)
+    {
+
+
+        if (go != null &&
+            isOff &&
+            lastObjectHit.GetComponent<ItemSelection>() != null &&
+            go.GetComponent<ItemSelection>() == null)
+        {
+            lastObjectHit.GetComponent<ItemSelection>().isOver = false;
+        }
+        isOff = false;
+
+    }
+
+
+
+
+}
