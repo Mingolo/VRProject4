2bedd4515b5c3ac94bd29d034c5c982ae6d7ee2c
Robert Kleszczynski <robert.ks.email@gmail.com>
2016-04-25 20:28:57 -0400

    80% Hospital done

---
+++ Assets/VR4 Assets/Scripts/HospitalMoving.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HospitalMoving : MonoBehaviour 
+{
+    public float speed = 3;
+    Vector3 moveAmount;
+    Vector3 forwardDir;
+    Rigidbody body;
+    bool isMoving = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+        body = this.GetComponent<Rigidbody>();
+        forwardDir = -Vector3.forward;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isMoving)
+        {
+            moveAmount = forwardDir * speed;
+        }
+       
+	}
+    void FixedUpdate()
+    {
+        body.MovePosition(this.transform.position + (moveAmount * Time.fixedDeltaTime));
+    }
+}
